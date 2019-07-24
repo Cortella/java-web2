b6fd9467f66d3c79c1edb9bce77117f6dc69f0de
Bruno Machado <nicolau.cortella@gmail.com>
2019-07-24 01:11:16 -0300

    Inicio Dev Web(Nao testado)

---
+++ src/br/com/cortella/controller/UsuarioController.java
@@ -0,0 +1,52 @@
+package br.com.cortella.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UsuarioController
+ */
+@WebServlet("/usucontroller.do")
+public class UsuarioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UsuarioController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String nome = request.getParameter("nome");
+		String empresa = resquest.getParameter("empresa");
+		System.out.println("Nome:" + nome);
+		System.out.println("Chamando metodo GET");
+		
+		PrintWriter saida = response.getWriter();
+		saida.println("Nome: " + nome + " Empresa: " + empresa);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		System.out.println("Chamando metodo POST");
+	}
+
+}
